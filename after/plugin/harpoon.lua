71ae6211137dda3867f75709cd1cc207e9b32cdf
mike <michael.p.vacha@gmail.com>
2024-01-03 22:59:12 -0600

    updated harpoon shortcuts to use alt key on left hand

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,10 +8,10 @@ harpoon:setup({
 vim.keymap.set("n", "<leader>s", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<a-a>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<a-s>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<a-d>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<a-f>", function() harpoon:list():select(4) end)
 
 vim.keymap.set("n", "<C-d>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-f>", function() harpoon:list():next() end)
