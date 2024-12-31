538a79b7a24047f53e79b43d0e92961b0e7a7b59
mike <michael.p.vacha@gmail.com>
2024-12-30 22:39:30 -0600

    removed trouble because I wasn't using it and it doesn't seem necessary

--- lua/mike/lazy/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "folke/trouble.nvim",
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-        {"<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)",},
-    },
-    -- seems I need to configure the fonts or symbols or something for this to work properly
-}
